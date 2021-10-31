09014e5cd0185f7e8671cffc29f79e81f2c7ce3c
Cade Williams <cadegw1@gmail.com>
2021-10-31 15:59:00 -0500

    imported necessary packages

--- song_popularity_predictor.py
+++ song_popularity_predictor.py
@@ -1,6 +1,22 @@
 # Aritifical Intelligence CSC-4444
 # Team 7 - Song Popularity Predictor
+import h5py
+import os
+import requests
+import numpy as np
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+
+client_id = os.environ['CLIENT_ID']
+client_secret = os.environ['CLIENT_SECRET']
+
+
+def authenticate():
+    pass
+
 
 if __name__ == '__main__':
     pass
+
 
