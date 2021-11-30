b86f5bbf91d175fa47c859fe06ab6a94e2e5087a
Cade Williams <cadegw1@gmail.com>
2021-11-29 23:26:18 -0600

    NN added able to plot values as intended but need to verify they match

--- predictor_pycaret.py
+++ predictor_pycaret.py
@@ -2,14 +2,23 @@
 from pycaret.regression import *
 import pandas as pd
 from IPython.display import display
+import matplotlib.pyplot as plt
+
+SAMPLE_SIZE = 1000
 
 data = pd.read_csv('SpotifyFeatures.csv')
-data = data.sample(100)
-regression = setup(data=data, target='popularity', session_id=100)
-print(models())
-display(compare_models(include=['lr', 'br', 'svm', 'ada', 'rf']))
-model = create_model('lr')
+data = data.sample(SAMPLE_SIZE)
+data['row_column'] = data.reset_index().index
+ax = data.plot(x='row_column', y='popularity')
+
+regression = setup(data=data, target='popularity', session_id=100, ignore_low_variance=True)
+# display(compare_models(include=['lr', 'br', 'svm', 'ada', 'rf']))
+model = create_model('rf')
 predictions = predict_model(model)
-display(predictions)
+
+predictions.plot(ax=ax, y='popularity')
+plt.show()
+
+# display(predictions)
 # interpret_model(model)
 
