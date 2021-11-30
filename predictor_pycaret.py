07224bad990b9cbd31626f72817633fc990119cc
Cade Williams <cadegw1@gmail.com>
2021-11-30 00:21:34 -0600

    NN progress towards plotting accuracy

--- predictor_pycaret.py
+++ predictor_pycaret.py
@@ -5,20 +5,29 @@ from IPython.display import display
 import matplotlib.pyplot as plt
 
 SAMPLE_SIZE = 1000
+TEST_SIZE = 50
 
-data = pd.read_csv('SpotifyFeatures.csv')
-data = data.sample(SAMPLE_SIZE)
-data['row_column'] = data.reset_index().index
-ax = data.plot(x='row_column', y='popularity')
+df = pd.read_csv('SpotifyFeatures.csv')
+data = df.iloc[0:SAMPLE_SIZE-1, :]
+test_data = df.iloc[SAMPLE_SIZE:SAMPLE_SIZE+TEST_SIZE-1, :]
+del df
 
-regression = setup(data=data, target='popularity', session_id=100, ignore_low_variance=True)
+regression = setup(data=data, target='popularity', session_id=100,
+                   ignore_low_variance=True, use_gpu=True, test_data=test_data)
 # display(compare_models(include=['lr', 'br', 'svm', 'ada', 'rf']))
-model = create_model('rf')
+model = create_model('lr')
 predictions = predict_model(model)
 
+print("Predictions\r\n")
+display(predictions['popularity'])
+print("Test Data\r\n")
+display(test_data['popularity'])
+
+# plot actual and target popularity scores
+test_data['row_column'] = test_data.reset_index().index
+ax = test_data.plot(x='row_column', y='popularity')
 predictions.plot(ax=ax, y='popularity')
 plt.show()
 
-# display(predictions)
 # interpret_model(model)
 
