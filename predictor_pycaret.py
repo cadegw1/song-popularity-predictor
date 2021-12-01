07bad545aaf7ce7d15856803a000d0c1ce08fd0c
Cade Williams <cadegw1@gmail.com>
2021-11-30 22:46:53 -0600

    NN have functional regression model

--- predictor_pycaret.py
+++ predictor_pycaret.py
@@ -1,33 +1,28 @@
 # from predictor_class import SongPopularityPredictor
 from pycaret.regression import *
 import pandas as pd
-from IPython.display import display
 import matplotlib.pyplot as plt
 
 SAMPLE_SIZE = 1000
 TEST_SIZE = 50
 
+# segmenting dataset
 df = pd.read_csv('SpotifyFeatures.csv')
-data = df.iloc[0:SAMPLE_SIZE-1, :]
-test_data = df.iloc[SAMPLE_SIZE:SAMPLE_SIZE+TEST_SIZE-1, :]
+data = df.iloc[0:SAMPLE_SIZE, :]
+test_data = df.iloc[SAMPLE_SIZE:SAMPLE_SIZE+TEST_SIZE, :]
 del df
 
-regression = setup(data=data, target='popularity', session_id=100,
-                   ignore_low_variance=True, use_gpu=True, test_data=test_data)
-# display(compare_models(include=['lr', 'br', 'svm', 'ada', 'rf']))
-model = create_model('lr')
-predictions = predict_model(model)
-
-print("Predictions\r\n")
-display(predictions['popularity'])
-print("Test Data\r\n")
-display(test_data['popularity'])
+# create, train, and test network
+regression = setup(data=data, target='popularity', session_id=100)
+# compare_models()
+model = create_model('br')  # bayesian ridge is most optimal according to compare_models()
+predictions = predict_model(estimator=model, data=test_data)
 
 # plot actual and target popularity scores
-test_data['row_column'] = test_data.reset_index().index
-ax = test_data.plot(x='row_column', y='popularity')
-predictions.plot(ax=ax, y='popularity')
+predictions['row_column'] = predictions.reset_index().index
+ax = predictions.plot(x='row_column', y=['popularity', 'Label'])    # Label represents predicted values
 plt.show()
+print(predictions[['popularity', 'Label', 'row_column']].head(TEST_SIZE))
 
 # interpret_model(model)
 
