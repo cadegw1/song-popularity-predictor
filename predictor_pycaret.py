17d6f89b6488893c856a8df0711455f21a594177
Cade Williams <cadegw1@gmail.com>
2021-11-30 23:26:25 -0600

    NN organized code and improved graph

--- predictor_pycaret.py
+++ predictor_pycaret.py
@@ -6,23 +6,39 @@ import matplotlib.pyplot as plt
 SAMPLE_SIZE = 1000
 TEST_SIZE = 50
 
+
 # segmenting dataset
-df = pd.read_csv('SpotifyFeatures.csv')
-data = df.iloc[0:SAMPLE_SIZE, :]
-test_data = df.iloc[SAMPLE_SIZE:SAMPLE_SIZE+TEST_SIZE, :]
-del df
+def segment_dataset(data, random=False):
+    if random is False:
+        sample = data.iloc[0:SAMPLE_SIZE, :]
+        test = data.iloc[SAMPLE_SIZE:SAMPLE_SIZE + TEST_SIZE, :]
+    else:
+        sample = data.sample(n=SAMPLE_SIZE)
+        test = data.sample(n=TEST_SIZE)
+    return sample, test
+
 
 # create, train, and test network
-regression = setup(data=data, target='popularity', session_id=100)
-# compare_models()
-model = create_model('br')  # bayesian ridge is most optimal according to compare_models()
-predictions = predict_model(estimator=model, data=test_data)
+def predict(train_data, test_data, target='popularity', regression_alg='br'):
+    setup(data=train_data, target=target, session_id=100)
+    model = create_model(regression_alg)  # bayesian ridge is most optimal according to compare_models()
+    return predict_model(estimator=model, data=test_data)
+
 
 # plot actual and target popularity scores
-predictions['row_column'] = predictions.reset_index().index
-ax = predictions.plot(x='row_column', y=['popularity', 'Label'])    # Label represents predicted values
-plt.show()
-print(predictions[['popularity', 'Label', 'row_column']].head(TEST_SIZE))
+def plot_accuracy(predictions, print_predictions=True):
+    predictions['row_column'] = predictions.reset_index().index
+    if print_predictions:
+        print(predictions[['popularity', 'Label', 'row_column']].head(TEST_SIZE))
+    predictions.plot(x='row_column', y=['popularity', 'Label'])    # Label represents predicted values
+    plt.xlabel("# of samples")
+    plt.ylabel("popularity")
+    plt.legend(['actual', 'predicted'])
+    plt.show()
 
-# interpret_model(model)
 
+if __name__ == '__main__':
+    df = pd.read_csv('SpotifyFeatures.csv')
+    train_data, test_data = segment_dataset(df, random=True)
+    predictions = predict(train_data, test_data)
+    plot_accuracy(predictions)
