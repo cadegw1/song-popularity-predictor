124c24d2721848a6ebddd05e389b69f0295f5d2f
Armando Castillo <acast58@lsu.edu>
2021-12-02 23:31:19 -0600

    Changed interpret to boolean

--- predictor_pycaret.py
+++ predictor_pycaret.py
@@ -6,8 +6,8 @@ import shap
 
 SAMPLE_SIZE = 1000
 TEST_SIZE = 50
-IGNORE_FEATURES_LIST = ["track_id","artist_name","track_name"]
-
+# IGNORE_FEATURES_LIST = ["track_id","artist_name","track_name"]
+#
 
 
 # segmenting dataset
@@ -22,13 +22,9 @@ def segment_dataset(data, random=False):
 
 # create, train, and test network
 def network(train_data, target, regression_alg):
-    setup(data=train_data, target=target, session_id=100, ignore_low_variance=True, ignore_features=IGNORE_FEATURES_LIST)
+    setup(data=train_data, target=target, session_id=100, ignore_low_variance=True)
     model = create_model(regression_alg)  # bayesian ridge is most optimal according to compare_models()
     return model
-
-# prediction
-def predict(model, test_data,):
-    return predict_model(estimator=model, data=test_data)
 
 # plot actual and target popularity scores
 def plot_accuracy(predictions, print_predictions=True):
@@ -46,15 +42,12 @@ if __name__ == '__main__':
     df = pd.read_csv('SpotifyFeatures.csv')
     target = 'popularity' #Target Feature
     regression_alg = 'dt'
-    genre_ignore = 'n' #can be (y,n)
-    interpret = 'y' #can be (y,n)
-    if genre_ignore is 'y':
-        IGNORE_FEATURES_LIST.append("genre")
+    interpret = True
 
     train_data, test_data = segment_dataset(df, random=True)
     network = network(train_data, target, regression_alg) #If using python kernel/jupyter -> Must Run 'def network' block to reset model.
-    predictions = predict(network, test_data)
+    predictions = predict_model(estimator=network, data=test_data)
 
     plot_accuracy(predictions)
-    if interpret is 'y':
+    if interpret is True:
         interpret_model(network)
